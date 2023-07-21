4bb1ae94501024d67e396c76e40eb966609e8714
Ellusive89 <>
2023-07-21 19:35:44 +0000

    posts and comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ellusive89-django-blog-i-a9xee8c2cc.us2.codeanyapp.com', 'django-blog-intro-c70bc73ef875.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    '8003-ellusive89-django-blog-i-a9xee8c2cc.us2.codeanyapp.com', 'localhost']
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
